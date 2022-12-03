b87388d8a2944e67cf0a5d03e48a653332fd75c2
Lisa Perlova <libertas.lis@yandex.ru>
2022-12-03 18:40:53 +0500

    tasks 64, 66, 68

---
+++ Task66/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте значения M и N. Напишите программу, которая 
+// найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите первое целое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе целое число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+void SumNumbers(int num1, int num2, int sum)
+{
+    sum = sum + num2;
+    if (num2 <= num1)
+    {
+        Console.Write($" {sum} ");
+        return;
+    }
+    SumNumbers(num1, num2 - 1, sum);
+
+    int temp = num1;
+    if (num1 > num2)
+    {
+        num1 = num2;
+        num2 = temp;
+    }
+}
+Console.Write($"M = {number1}, N = {number2} -> ");
+SumNumbers(number1, number2, 0);
---
+++ Task68/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу вычисления функции Аккермана 
+// с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.Write("Введите первое целое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе целое число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int Akkerman(int num1, int num2)
+{
+if (num1 == 0) return num2 + 1;
+else if (num2 == 0 && num1 > 0) return Akkerman(num1 - 1, 1);
+else return Akkerman(num1 - 1, Akkerman(num1, num2 - 1));
+}
+
+int akkermanFunktion = Akkerman (number1, number2);
+Console.Write($"m = {number1}, n = {number2} -> A({number1},{number2}) = {akkermanFunktion}");
